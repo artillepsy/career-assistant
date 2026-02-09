529d2c46ddce8d98d39f53620d3dfb00f8022f73
Artillepsy <artillepsy@gmail.com>
2026-02-09 14:48:49 +0100

    test: add basic assertion in CareerAssistantApiApplicationTests

--- backend/career-assistant-api/src/test/java/com/artillepsy/career_assistant_api/CareerAssistantApiApplicationTests.java
+++ backend/career-assistant-api/src/test/java/com/artillepsy/career_assistant_api/CareerAssistantApiApplicationTests.java
@@ -1,8 +1,11 @@
 package com.artillepsy.career_assistant_api;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
@@ -10,6 +13,7 @@ class CareerAssistantApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
